c6337cd42dce310e27deabe0990642defa0bf920
sasfmlzr <sasfmlzr@yandex.ru>
2018-10-01 13:41:13 +0300

    Initial commit

---
+++ app/src/main/java/com/sasfmlzr/distillery/dvachmovie/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sasfmlzr.distillery.dvachmovie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sasfmlzr.distillery.dvachmovie.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
