eea7a53b9ed609b60ebe0d339572901f0908ef12
sasfmlzr <sasfmlzr@yandex.ru>
2018-10-01 14:49:38 +0300

    Add player and databinding.

--- app/src/main/java/com/sasfmlzr/distillery/dvachmovie/ui/main/MainFragment.kt
+++ app/src/main/java/com/sasfmlzr/distillery/dvachmovie/ui/main/MainFragment.kt
@@ -6,25 +6,33 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sasfmlzr.distillery.dvachmovie.R
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+import com.sasfmlzr.distillery.dvachmovie.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+
+    private lateinit var player : SimpleExoPlayer
+    private lateinit var playerView: PlayerView
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding : MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        binding.viewmodel = viewModel
 
+        player = ExoPlayerFactory.newSimpleInstance(context)
+        playerView = binding.playerView
+        playerView.player = player;
+        
+        return binding.root
+    }
 }
