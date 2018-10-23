eca8633c2a9418b21d3a7dd4490c696824e696b7
sasfmlzr <sasfmlzr@yandex.ru>
2018-10-23 15:42:13 +0300

    Fix threadRequest result

--- domain/src/main/kotlin/dvachmovie/request/DvachMovieApi.kt
+++ domain/src/main/kotlin/dvachmovie/request/DvachMovieApi.kt
@@ -1,6 +1,7 @@
 package dvachmovie.request
 
 import dvachmovie.request.model.catalog.DvachCatalogRequest
+import dvachmovie.request.model.thread.DvachThreadRequest
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,5 +12,5 @@ interface DvachMovieApi {
 
     @GET("https://2ch.hk/{board}/res/{numThread}.json")
     fun getThread(@Path("board") board: String,
-                   @Path("numThread") numThread: String): Call<DvachCatalogRequest>
+                   @Path("numThread") numThread: String): Call<DvachThreadRequest>
 }
