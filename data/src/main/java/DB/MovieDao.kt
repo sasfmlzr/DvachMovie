3439f4e38e5b81912577770e0c4d7427bab66b18
sasfmlzr <sasfmlzr@yandex.ru>
2018-10-25 15:05:07 +0300

    Set rx for room dao

--- data/src/main/java/DB/MovieDao.kt
+++ data/src/main/java/DB/MovieDao.kt
@@ -4,11 +4,12 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy.IGNORE
 import android.arch.persistence.room.Query
+import io.reactivex.Flowable
 
 @Dao
 interface MovieDao {
     @Query("SELECT * from movieData")
-    fun getAll(): List<MovieEntity>
+    fun getAll(): Flowable<MovieEntity>
 
     //@Query("SELECT * FROM movieData WHERE movieLink = :link")
     //fun linkIsExist(link: String): Flowable<User>
