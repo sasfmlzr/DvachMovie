bcff8002c9fa06a5eb60fa42771a4cf96766fb39
sasfmlzr <sasfmlzr@yandex.ru>
2018-11-09 16:53:22 +0300

    Add MovieTempRepository

--- data/src/main/java/dvachmovie/di/DataModule.kt
+++ data/src/main/java/dvachmovie/di/DataModule.kt
@@ -1,7 +1,13 @@
 package dvachmovie.di
 
 import dagger.Module
+import dagger.Provides
+import dvachmovie.repository.local.MovieTempRepository
+import javax.inject.Singleton
 
 @Module(includes = arrayOf(RoomModule::class, ApiModule::class))
 class DataModule {
+    @Singleton
+    @Provides
+    fun movieTempListRepository() = MovieTempRepository()
 }
---
+++ data/src/main/java/dvachmovie/repository/local/MovieTempRepository.kt
@@ -0,0 +1,3 @@
+package dvachmovie.repository.local
+
+data class MovieTempRepository(val movieLists: List<String> = listOf())
