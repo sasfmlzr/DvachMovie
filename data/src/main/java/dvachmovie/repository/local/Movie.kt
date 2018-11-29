6b42e10ce932f1e412ca41d7498ee426175c2f60
sasfmlzr <sasfmlzr@yandex.ru>
2018-11-29 16:22:58 +0300

    Delete Parcelable for movie model

--- data/src/main/java/dvachmovie/repository/local/Movie.kt
+++ data/src/main/java/dvachmovie/repository/local/Movie.kt
@@ -1,31 +1,4 @@
 package dvachmovie.repository.local
 
-import android.os.Parcel
-import android.os.Parcelable
-
 data class Movie(val movieUrl: String = "",
-                 val moviePreviewUrl: String = "") : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString())
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(movieUrl)
-        parcel.writeString(moviePreviewUrl)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+                 val moviePreviewUrl: String = "")
