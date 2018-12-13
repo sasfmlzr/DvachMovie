650d3df114f98955330ccd2510e49c1ff4a6e4a9
sasfmlzr <sasfmlzr@yandex.ru>
2018-12-13 18:58:37 +0300

    Edit logic between ui and room

--- data/src/main/java/dvachmovie/repository/local/MovieRepository.kt
+++ data/src/main/java/dvachmovie/repository/local/MovieRepository.kt
@@ -24,7 +24,41 @@ class MovieRepository @Inject constructor(
 
     fun observe(lifecycleOwner: LifecycleOwner) {
         movieDBRepository.getAll().observe(lifecycleOwner, Observer {
-            movieStorage.movieList.value = it as MutableList<MovieEntity>
+
+            val list =calculateDiff(movieStorage.movieList.value!!, it as MutableList<MovieEntity>)
+            if (list.size!=0) {
+                val movieTempList = movieStorage.movieList.value
+                movieTempList!!.addAll(list)
+                movieStorage.movieList.value = movieTempList
+            }
         })
     }
+
+    private fun calculateDiff(localList: MutableList<MovieEntity>,
+                              dbList: MutableList<MovieEntity>) :
+            MutableList<MovieEntity> {
+        val movieList = dbList
+        val result = mutableListOf<MovieEntity>()
+
+        localList.map { movie ->
+            movieList.contains(movie).let {
+                movieList.remove(movie)
+            }
+        }
+
+        movieList.map { movie ->
+            var equals = false
+            localList.map { localMovie ->
+                if (localMovie.movieUrl == movie.movieUrl) {
+                    equals = true
+                }
+            }
+            if (!equals) {
+                result.add(movie)
+            }
+        }
+        return result
+    }
+
+
 }
