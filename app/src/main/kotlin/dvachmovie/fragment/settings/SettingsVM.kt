5f94060f7d2614045fe9580b9a41bdbb8746bbf3
sasfmlzr <sasfmlzr@yandex.ru>
2018-12-26 14:53:01 +0300

    Add alert dialog for button

--- app/src/main/kotlin/dvachmovie/fragment/settings/SettingsVM.kt
+++ app/src/main/kotlin/dvachmovie/fragment/settings/SettingsVM.kt
@@ -1,24 +1,45 @@
 package dvachmovie.fragment.settings
 
+import android.content.DialogInterface
+import android.view.View
 import android.widget.CompoundButton
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dvachmovie.repository.db.MovieDBRepository
 import dvachmovie.usecase.SettingsUseCase
 import javax.inject.Inject
 
 class SettingsVM @Inject constructor(
+        private val movieRepository: MovieDBRepository,
         settingsUseCase: SettingsUseCase
 ) : ViewModel() {
 
     val prepareLoading = MutableLiveData<Boolean>()
 
+    val onRefreshDB = MutableLiveData<Boolean>()
+
     init {
         prepareLoading.value = settingsUseCase.getLoadingParam()
+        onRefreshDB.value = false
     }
 
     val onPrepareLoadingClicked =
             CompoundButton.OnCheckedChangeListener { _, isChecked ->
         prepareLoading.value = isChecked
     }
+
+    val onRefreshDatabase =
+            View.OnClickListener {
+                AlertDialog.Builder(it.context)
+                        .setTitle("Confirmation")
+                        .setMessage("Database will clean")
+                        .setPositiveButton("Ok") { _, _ ->
+                            println("refresh")
+                            onRefreshDB.value = true
+                        }
+                        .setNegativeButton("Cancel") {_,_ ->}
+                        .show()
+            }
 
 }
