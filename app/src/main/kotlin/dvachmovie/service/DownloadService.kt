71cbd8afe9a1f56e50f714c8d128dd8f82224854
sasfmlzr <sasfmlzr@yandex.ru>
2019-01-09 13:54:32 +0300

    Filename show in notification.

--- app/src/main/kotlin/dvachmovie/service/DownloadService.kt
+++ app/src/main/kotlin/dvachmovie/service/DownloadService.kt
@@ -32,7 +32,7 @@ class DownloadService : IntentService(Context.DOWNLOAD_SERVICE) {
         val request = DownloadManager.Request(uri)
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)  // Tell on which network you want to download file.
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)  // This will show notification on top when downloading the file.
-        request.setTitle("Downloading a file") // Title for notification.
+        request.setTitle(uri.pathSegments.last()) // Title for notification.
         request.setVisibleInDownloadsUi(true)
         request.setDestinationInExternalPublicDir(destinationPath, uri.lastPathSegment)  // Storage directory path
         (getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager).enqueue(request) // This will start downloading
