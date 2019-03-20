5aa5242442060c5640003c9db561431cf93c16fe
sasfmlzr <sasfmlzr@yandex.ru>
2019-03-20 22:09:41 +0300

    Add md5 for fileItem.

--- data/src/main/kotlin/dvachmovie/api/model/thread/FileItem.kt
+++ data/src/main/kotlin/dvachmovie/api/model/thread/FileItem.kt
@@ -3,28 +3,34 @@ package dvachmovie.api.model.thread
 import com.google.gson.annotations.SerializedName
 
 data class FileItem(@SerializedName("tn_height")
-                     val tnHeight: Int = 0,
+                    val tnHeight: Int = 0,
                     @SerializedName("thumbnail")
-                     val thumbnail: String = "",
+                    val thumbnail: String = "",
                     @SerializedName("sticker")
-                     val sticker: String = "",
+                    val sticker: String = "",
                     @SerializedName("type")
-                     val type: Int = 0,
+                    val type: Int = 0,
                     @SerializedName("pack")
-                     val pack: String = "",
+                    val pack: String = "",
                     @SerializedName("path")
-                     val path: String = "",
+                    val path: String = "",
                     @SerializedName("tn_width")
-                     val tnWidth: Int = 0,
+                    val tnWidth: Int = 0,
                     @SerializedName("size")
-                     val size: Int = 0,
+                    val size: Int = 0,
                     @SerializedName("install")
-                     val install: String = "",
+                    val install: String = "",
                     @SerializedName("displayname")
-                     val displayname: String = "",
+                    val displayname: String = "",
                     @SerializedName("name")
-                     val name: String = "",
+                    val name: String = "",
                     @SerializedName("width")
-                     val width: Int = 0,
+                    val width: Int = 0,
                     @SerializedName("height")
-                     val height: Int = 0)
+                    val height: Int = 0,
+                    @SerializedName("fullname")
+                    val fullname: String = "",
+                    @SerializedName("md5")
+                    val md5: String = "",
+                    @SerializedName("nsfw")
+                    val nsfw: Int = 0)
