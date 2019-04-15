0a7ca011f6482de1211bb123266f84df6789f9ac
sasfmlzr <sasfmlzr@yandex.ru>
2019-04-15 14:03:41 +0300

    Binding improvements.

--- app/src/main/kotlin/dvachmovie/architecture/binding/BindingAdapters.kt
+++ app/src/main/kotlin/dvachmovie/architecture/binding/BindingAdapters.kt
@@ -3,25 +3,23 @@ package dvachmovie.architecture.binding
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.LazyHeaders
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestOptions
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import dvachmovie.api.Cookie
 import dvachmovie.db.data.MovieEntity
 
-@BindingAdapter("imageFromUrl", "cookie")
+@BindingAdapter("imageFromUrl", "cookie", requireAll = true)
 fun ImageView.bindImageFromUrl(movieEntity: MovieEntity, cookie: Cookie) {
-    if (!movieEntity.previewUrl.isEmpty()) {
+    if (movieEntity.previewUrl.isNotEmpty()) {
         val builder = LazyHeaders.Builder().addHeader("Cookie", cookie.toString())
 
         val glideUrl = GlideUrl(movieEntity.previewUrl, builder.build())
 
-        Glide.with(this.context)
+        Glide.with(this)
                 .load(glideUrl)
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(this)
@@ -29,12 +27,11 @@ fun ImageView.bindImageFromUrl(movieEntity: MovieEntity, cookie: Cookie) {
 }
 
 @BindingAdapter("imageFromResource")
-fun bindImageFromGif(view: ImageView, resourceId: Int) {
-    Glide.with(view.context)
+fun ImageView.bindImageFromResource(resourceId: Int) {
+    Glide.with(this)
             .load(resourceId)
-            .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
             .transition(DrawableTransitionOptions.withCrossFade())
-            .into(view)
+            .into(this)
 }
 
 @BindingAdapter("showBanner")
