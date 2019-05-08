ca21c826e5142df588b11e639a81d72c28bbec63
sasfmlzr <sasfmlzr@yandex.ru>
2019-05-08 15:08:16 +0300

    Add cookie model test.

---
+++ domain/src/test/kotlin/dvachmovie/api/CookieModelTest.kt
@@ -0,0 +1,13 @@
+package dvachmovie.api
+
+import org.junit.Assert
+import org.junit.Test
+
+class CookieModelTest {
+
+    @Test
+    fun `Verify cookie model` (){
+        val cookie = Cookie("var", "val")
+        Assert.assertEquals(cookie.toString(), "var=val")
+    }
+}
