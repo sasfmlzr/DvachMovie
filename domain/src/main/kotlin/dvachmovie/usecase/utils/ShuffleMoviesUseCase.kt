fc66a43e5d0be4bd4fffd63a5ea7d10af5015d2e
sasfmlzr <sasfmlzr@yandex.ru>
2019-06-03 17:34:14 +0300

    Add test for shuffle movies pipe.

--- domain/src/main/kotlin/dvachmovie/usecase/utils/ShuffleMoviesUseCase.kt
+++ domain/src/main/kotlin/dvachmovie/usecase/utils/ShuffleMoviesUseCase.kt
@@ -5,7 +5,7 @@ import dvachmovie.usecase.base.UseCase
 import dvachmovie.utils.MovieUtils
 import javax.inject.Inject
 
-class ShuffleMoviesUseCase @Inject constructor(
+open class ShuffleMoviesUseCase @Inject constructor(
         private val movieUtils: MovieUtils) : UseCase<List<Movie>, List<Movie>>() {
 
     override suspend fun executeAsync(input: List<Movie>): List<Movie> =
--- presenter/src/main/kotlin/dvachmovie/pipe/utils/ShuffleMoviesPipe.kt
+++ presenter/src/main/kotlin/dvachmovie/pipe/utils/ShuffleMoviesPipe.kt
@@ -1,23 +1,18 @@
 package dvachmovie.pipe.utils
 
 import dvachmovie.PresenterModel
-import dvachmovie.architecture.ScopeProvider
-import dvachmovie.db.data.Movie
 import dvachmovie.architecture.PipeAsync
+import dvachmovie.db.data.Movie
 import dvachmovie.pipe.ShuffledMoviesModel
 import dvachmovie.usecase.utils.ShuffleMoviesUseCase
 import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ShuffleMoviesPipe @Inject constructor(
         private val broadcastChannel: BroadcastChannel<PresenterModel>,
-        private val useCase: ShuffleMoviesUseCase,
-        private val scopeProvider: ScopeProvider) : PipeAsync<List<Movie>>() {
+        private val useCase: ShuffleMoviesUseCase) : PipeAsync<List<Movie>>() {
 
     override suspend fun execute(input: List<Movie>) {
-        scopeProvider.ioScope.launch {
-            broadcastChannel.send(ShuffledMoviesModel(useCase.executeAsync(input)))
-        }
+        broadcastChannel.send(ShuffledMoviesModel(useCase.executeAsync(input)))
     }
 }
