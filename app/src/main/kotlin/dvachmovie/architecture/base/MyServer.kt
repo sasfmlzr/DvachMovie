77086fba116d466524f2ed55eebd8e8718744afe
sasfmlzr <sasfmlzr@yandex.ru>
2019-06-04 16:05:52 +0300

    Detekt improvements.

--- app/src/main/kotlin/dvachmovie/architecture/base/MyServer.kt
+++ app/src/main/kotlin/dvachmovie/architecture/base/MyServer.kt
@@ -6,10 +6,9 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 import dvachmovie.architecture.logging.Logger
 
-
 class MyServer (val logger: Logger) : LifecycleObserver{
     @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
     fun onAny(source: LifecycleOwner, event: Lifecycle.Event ){
-        logger.d(source.javaClass.name, "WWWWWW ${source.javaClass.name} ${event}")
+        logger.d(source.javaClass.name, "${event}")
     }
 }
