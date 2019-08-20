38f7e4acf31e00891e1a3dd7996250f2dcb256bd
sasfmlzr <sasfmlzr@yandex.ru>
2019-08-20 14:14:31 +0300

    Move deleting movies logic to start activity. Add test for delete old movies pipe.

--- app/src/main/kotlin/dvachmovie/activity/movie/MovieActivityVM.kt
+++ app/src/main/kotlin/dvachmovie/activity/movie/MovieActivityVM.kt
@@ -1,13 +1,8 @@
 package dvachmovie.activity.movie
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dvachmovie.AppConfig
-import dvachmovie.pipe.db.DeleteOldMoviesPipe
 import dvachmovie.pipe.settingsstorage.GetCurrentBaseUrlPipe
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MovieActivityVM @Inject constructor() : ViewModel() {
@@ -15,16 +10,7 @@ class MovieActivityVM @Inject constructor() : ViewModel() {
     @Inject
     lateinit var getCurrentBaseUrlPipe: GetCurrentBaseUrlPipe
 
-    @Inject
-    lateinit var deleteOldMoviesPipe: DeleteOldMoviesPipe
-
     fun initCurrentBaseUrl() {
         AppConfig.currentBaseUrl = getCurrentBaseUrlPipe.execute(Unit)
-
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                deleteOldMoviesPipe.execute(Unit)
-            }
-        }
     }
 }
--- app/src/main/kotlin/dvachmovie/activity/start/StartActivityVM.kt
+++ app/src/main/kotlin/dvachmovie/activity/start/StartActivityVM.kt
@@ -2,10 +2,25 @@ package dvachmovie.activity.start
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dvachmovie.AppConfig
 import dvachmovie.BuildConfig
+import dvachmovie.pipe.db.DeleteOldMoviesPipe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class StartActivityVM @Inject constructor() : ViewModel() {
     val initText = MutableLiveData("Preparing...")
     val version = MutableLiveData(BuildConfig.VERSION_NAME)
+
+    @Inject
+    lateinit var deleteOldMoviesPipe: DeleteOldMoviesPipe
+
+     suspend fun removeOldMovies() {
+            withContext(Dispatchers.IO) {
+                deleteOldMoviesPipe.execute(Unit)
+            }
+    }
 }
