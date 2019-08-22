e01c235667f376ff71c910a69bd64a2d3561bb21
sasfmlzr <sasfmlzr@yandex.ru>
2019-08-22 14:14:41 +0300

    Add tests for neoChan

--- domain/src/main/kotlin/dvachmovie/db/data/NullMovie.kt
+++ domain/src/main/kotlin/dvachmovie/db/data/NullMovie.kt
@@ -11,4 +11,31 @@ data class NullMovie(override val movieUrl: String = "",
                      override val post: Long = 0,
                      override val thread: Long = 0,
                      override val baseUrl: String = "",
-                     override var dateAddedToDB: LocalDateTime = LocalDateTime()) : Movie
+                     override var dateAddedToDB: LocalDateTime = LocalDateTime()) : Movie {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NullMovie
+
+        if (movieUrl != other.movieUrl) return false
+        if (previewUrl != other.previewUrl) return false
+        if (board != other.board) return false
+        if (isPlayed != other.isPlayed) return false
+        if (thread != other.thread) return false
+        if (baseUrl != other.baseUrl) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = movieUrl.hashCode()
+        result = 31 * result + previewUrl.hashCode()
+        result = 31 * result + board.hashCode()
+        result = 31 * result + isPlayed.hashCode()
+        result = 31 * result + thread.hashCode()
+        result = 31 * result + baseUrl.hashCode()
+        return result
+    }
+}
---
+++ domain/src/test/kotlin/dvachmovie/usecase/real/neochan/GetLinkFilesFromThreadsNeoChanUseCaseTest.kt
@@ -0,0 +1,55 @@
+package dvachmovie.usecase.real.neochan
+
+import dvachmovie.TestException
+import dvachmovie.api.FileItem
+import dvachmovie.architecture.logging.Logger
+import dvachmovie.repository.NeoChanRepository
+import dvachmovie.usecase.real.GetLinkFilesFromThreadsUseCaseModel
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+internal class GetLinkFilesFromThreadsNeoChanUseCaseTest {
+
+    @InjectMocks
+    private lateinit var useCase: GetLinkFilesFromThreadsNeoChanUseCase
+
+    @Mock
+    private lateinit var logger: Logger
+
+    @Mock
+    private lateinit var neoChanRepository: NeoChanRepository
+
+    private val testValue = FileItem("test")
+
+    private val listNumThreads = listOf(testValue)
+
+    private val testException = TestException()
+
+    private val testBoard = "testBoard"
+
+    private val model = GetLinkFilesFromThreadsNeoChanUseCase.Params(testBoard, "numThread", "nameThread")
+
+    @Test
+    fun `Happy pass`() {
+        runBlocking {
+            given(neoChanRepository.getConcreteThreadByNum(testBoard, "numThread", "nameThread")).willReturn(listNumThreads)
+            Assert.assertEquals(GetLinkFilesFromThreadsUseCaseModel(listNumThreads),
+                    useCase.executeAsync(model))
+        }
+    }
+
+    @Test(expected = TestException::class)
+    fun `Get concrete thread by num throw exception`() {
+        runBlocking {
+            given(neoChanRepository.getConcreteThreadByNum(testBoard, "numThread", "nameThread")).willThrow(testException)
+            useCase.executeAsync(model)
+        }
+    }
+}
