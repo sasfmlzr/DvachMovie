94326c9197e133a028eb277a5dd5a8cfeca4bfa9
sasfmlzr <sasfmlzr@yandex.ru>
2019-11-19 14:10:46 +0300

    Update third dependencies

--- domain/src/test/kotlin/dvachmovie/usecase/db/InsertionMovieToDBUseCaseTest.kt
+++ domain/src/test/kotlin/dvachmovie/usecase/db/InsertionMovieToDBUseCaseTest.kt
@@ -1,9 +1,11 @@
 package dvachmovie.usecase.db
 
 import dvachmovie.TestException
+import dvachmovie.db.data.Movie
 import dvachmovie.db.data.NullMovie
 import dvachmovie.repository.MovieDBRepository
 import kotlinx.coroutines.runBlocking
+import org.joda.time.LocalDateTime
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.BDDMockito.given
@@ -22,19 +24,33 @@ class InsertionMovieToDBUseCaseTest {
 
     private val movie = NullMovie("test")
 
+    private val movieTwo = object : Movie {
+        override val movieUrl: String= ""
+        override val previewUrl: String= ""
+        override val board: String= ""
+        override var isPlayed: Boolean = false
+        override var date: LocalDateTime = LocalDateTime()
+        override val md5: String= ""
+        override val post: Long = 0
+        override val thread: Long = 0
+        override val baseUrl: String= ""
+        override var dateAddedToDB: LocalDateTime = LocalDateTime()
+    }
+
     @Test
     fun `Happy pass`() {
         runBlocking {
             useCase.executeAsync(movie)
+            useCase.executeAsync(movieTwo)
         }
     }
 
     @Test(expected = TestException::class)
     fun `Something was wrong`() {
         runBlocking {
-            given(movieDBRepository.insert(movie)).willThrow(TestException())
+            given(movieDBRepository.insert(movieTwo)).willThrow(TestException())
 
-            useCase.executeAsync(movie)
+            useCase.executeAsync(movieTwo)
         }
     }
 }
