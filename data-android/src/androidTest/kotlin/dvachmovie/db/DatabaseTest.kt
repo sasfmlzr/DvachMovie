3f7edd0c33bc505ee11f34b740683b360af57fc2
sasfmlzr <sasfmlzr@yandex.ru>
2019-11-21 17:14:15 +0300

    Add DB tests

--- data-android/src/androidTest/kotlin/dvachmovie/db/DatabaseTest.kt
+++ data-android/src/androidTest/kotlin/dvachmovie/db/DatabaseTest.kt
@@ -6,19 +6,23 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dvachmovie.db.model.MovieDao
 import dvachmovie.db.model.MovieEntity
+import dvachmovie.db.model.ThreadDao
+import dvachmovie.db.model.ThreadEntity
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.joda.time.LocalDateTime
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
-class SimpleEntityReadWriteTest {
-    private lateinit var movieDao: MovieDao
+class DatabaseTest {
     private lateinit var db: MovieDatabase
+    private lateinit var movieDao: MovieDao
+    private lateinit var threadDao: ThreadDao
 
     @Before
     fun createDb() {
@@ -26,6 +30,7 @@ class SimpleEntityReadWriteTest {
         db = Room.inMemoryDatabaseBuilder(
                 context, MovieDatabase::class.java).build()
         movieDao = db.movieDao()
+        threadDao = db.threadDao()
     }
 
     @After
@@ -36,7 +41,7 @@ class SimpleEntityReadWriteTest {
 
     @Test
     @Throws(Exception::class)
-    fun writeUserAndReadInList() {
+    fun happyPassAndroidMovieData() {
         val baseUrl = "testBaseUrl"
         val testBoard = "testBoard"
         val movie = MovieEntity("movieUrl", "previewUrl", testBoard,
@@ -44,6 +49,24 @@ class SimpleEntityReadWriteTest {
         movieDao.insertAll(listOf(movie))
         val newMovie = movieDao.getMoviesFromBoard(baseUrl, testBoard)
         assertThat(newMovie[0], equalTo(movie))
-        println("Movie test finished")
+        Assert.assertEquals(newMovie[0].hashCode(), movie.hashCode())
+        Assert.assertEquals(newMovie[0].baseUrl, movie.baseUrl)
+        Assert.assertNotEquals(false, movie.baseUrl)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun happyPassAndroidThreadData() {
+        val threadNumber = "10"
+        val baseUrl = "testBaseUrl"
+        val boardThread = "testThread"
+        val thread = ThreadEntity(threadNumber.toLong(), LocalDateTime(), false,
+                boardThread, baseUrl)
+        threadDao.insertAll(listOf(thread))
+        val newThread = threadDao.getThreadByNum(threadNumber, baseUrl)
+        assertThat(newThread, equalTo(thread))
+        Assert.assertEquals(newThread.hashCode(), thread.hashCode())
+        Assert.assertEquals(newThread?.baseUrl, thread.baseUrl)
+        Assert.assertNotEquals(false, thread.baseUrl)
     }
 }
